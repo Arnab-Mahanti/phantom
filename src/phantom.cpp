d12948467e11aadd3809c028e20ec0cb348b40d6
Arnab-Mahanti <mahanti.mtg@gmail.com>
2023-03-05 18:31:22 +0530

    fixes for proper distribution

--- src/phantom.cpp
+++ src/phantom.cpp
@@ -15,14 +15,16 @@ int main()
     auto grid_size = 10;
     auto grid_refinement = 5;
     auto modes = 3;
-    auto nos = 10;
+    auto nos = 10000;
     auto T_min = 1000.0;
     auto T_max = 2000.0;
     std::string filename = "MAMLAS_test_gaussian_3_2d.hdf5";
 
     std::random_device entropy_source;
     std::mt19937 mt(entropy_source());
-    auto dist = std::uniform_real_distribution<double>();
+    auto dist13 = std::uniform_real_distribution<double>(0.15, 0.35);
+    auto dist19 = std::uniform_real_distribution<double>(0.1, 0.9);
+    auto dist37 = std::uniform_real_distribution<double>(0.3, 0.7);
 
     auto T_data = std::vector(nos,
                               matplot::vector_2d(grid_refinement * grid_size,
@@ -35,10 +37,10 @@ int main()
     {
         for (size_t n = 0; n < modes; n++)
         {
-            auto peak_x = dist(mt);
-            auto peak_y = dist(mt);
-            auto peak_variance = dist(mt);
-            auto peak_magnitude = dist(mt);
+            auto peak_x = dist19(mt);
+            auto peak_y = dist19(mt);
+            auto peak_variance = dist13(mt);
+            auto peak_magnitude = dist37(mt);
             auto t = matplot::transform(X, Y, [&](auto x, auto y)
                                         { return peak_magnitude * std::exp(-((x - peak_x) * (x - peak_x) + (y - peak_y) * (y - peak_y)) / (2 * peak_variance * peak_variance)); });
             T = matplot::transform(T, t, [](auto a, auto b)
@@ -49,9 +51,9 @@ int main()
     }
 
     auto file = h5::File(filename, h5::File::Create | h5::File::Overwrite);
-    file.createDataSet("data",T_data);
-    file.createDataSet("T_min",T_min);
-    file.createDataSet("T_max",T_max);
-    matplot::surf(X, Y, T_data[0]);
+    file.createDataSet("data", T_data);
+    file.createDataSet("T_min", T_min);
+    file.createDataSet("T_max", T_max);
+    matplot::surf(X, Y, T_data[std::uniform_int_distribution(0, nos)(mt)]);
     matplot::show();
 }
